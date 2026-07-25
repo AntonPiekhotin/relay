5dcad6a28effe1e70a5f1898742c4cecaba5276a
apiekhotin <apiekhotin@velipayment.com>
2026-07-25 22:07:43 +0200

    Remove ignored files from tracking

---
+++ common/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    `maven-publish`
+}
+
+group = "com.relay"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["kotlin"])
+        }
+    }
+}
