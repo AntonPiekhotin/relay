710eb89b564527a9bc34f2f9594ac3f33a808dc6
apiekhotin <apiekhotin@velipayment.com>
2026-07-25 22:53:37 +0200

    implement eureka server

--- eureka/build.gradle.kts
+++ eureka/build.gradle.kts
@@ -20,8 +20,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:5.0.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
